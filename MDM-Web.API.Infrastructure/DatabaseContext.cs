d5de22b531f59f61de15a19c7afdeb40c0a2ca4c
Filip Ceglik <fceglik@gmail.com>
2021-04-14 21:23:56 +0200

    added dbcontext for mongoDB

---
+++ MDM-Web.API.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace MDM_Web.API.Infrastructure
+{
+    public class DatabaseContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public DatabaseContext(string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase("MDM");
+        }
+
+        public IMongoCollection<TModel> GetCollection<TModel>() => _database.GetCollection<TModel>(typeof(TModel).Name);
+    }
+}
