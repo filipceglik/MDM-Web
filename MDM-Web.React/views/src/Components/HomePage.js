887f58b77cd2309ec60471f72923995ebeb91ed5
Paweł Skalski <skalskip@wp.pl>
2021-06-06 13:23:20 +0200

    Created homepage component

---
+++ MDM-Web.React/views/src/Components/HomePage.js
@@ -0,0 +1,18 @@
+﻿import './App.css';
+import 'bootstrap'
+import 'react-bootstrap'
+import "./css/Login.css"
+
+function Login(){
+
+    return(
+        <div className="container Login">
+            <div className="row">
+                <div className="col-12">
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+}
