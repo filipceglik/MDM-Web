5ddafadd573b1a8e2635df74244f40f9cb4e3b3e
Paweł Skalski <skalskip@wp.pl>
2021-06-20 11:24:08 +0200

    Create 404 error handler

--- MDM-Web.React/views/src/Routes.js
+++ MDM-Web.React/views/src/Routes.js
@@ -2,12 +2,18 @@
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import Login from "./Login";
+import NotFound from "./Components/NotFound";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Login />
+            </Route>
+            
+            {/* Finally, catch all unmatched routes */}
+            <Route>
+                <NotFound />
             </Route>
         </Switch>
     );
