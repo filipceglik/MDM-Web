de9ad01e3fafc8eb08cfd1c8d0417bedcd8b5a1b
Paweł Skalski <skalskip@wp.pl>
2021-06-23 14:29:47 +0200

    Created backbone of show device page

--- MDM-Web.React/views/src/Routes.js
+++ MDM-Web.React/views/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import NotFound from "./Components/NotFound";
 import Login from "./Components/Login";
 import HomePage from "./Components/HomePage";
+import Device from "./Components/Device";
 
 export default function Routes() {
     return (
@@ -12,6 +13,9 @@ export default function Routes() {
             </Route>
             <Route exact path="/login">
                 <Login />
+            </Route>
+            <Route exact path="/device">
+                <Device />
             </Route>
             {/* Finally, catch all unmatched routes */}
             <Route>
