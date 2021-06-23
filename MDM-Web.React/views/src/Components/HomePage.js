4766ad3754d1a95fc2d81db234b3c23b18b37d71
Paweł Skalski <skalskip@wp.pl>
2021-06-23 14:03:02 +0200

    Created data binding for devices list, currently without API connection

--- MDM-Web.React/views/src/Components/HomePage.js
+++ MDM-Web.React/views/src/Components/HomePage.js
@@ -1,8 +1,10 @@
 ﻿import 'bootstrap'
 import 'react-bootstrap'
 import '../css/HomePage.css'
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useAppContext} from "../libs/contextLib";
+import {LinkContainer} from "react-router-bootstrap";
+import {ListGroup} from "react-bootstrap";
 
 
 function HomePage() {
@@ -10,8 +12,51 @@ function HomePage() {
     const {isAuthenticated} = useAppContext();
     const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        function onLoad() {
+            if (!isAuthenticated) {
+                return;
+            }
+
+            try {
+                setDevices(loadDevices)
+            } catch (e) {
+                alert("Nie się pobrać listy urządzeń, odśwież stronę aby spróbować ponownie")
+            }
+
+            setIsLoading(false);
+        }
+
+        onLoad();
+    }, [isAuthenticated]);
+
+    function loadDevices() {
+        return [{
+            deviceID: 1,
+            Name: "Urządzenie Filipa",
+            SystemName: "IOS",
+            SystemVersion: "13",
+            Model: "Iphone 11",
+            BatteryState: 99,
+            BatteryLevel: 69
+        }]
+    }
+
     function renderDevicesList(devices) {
-        return null
+        return (
+            <>
+                {devices.map(({deviceID, Name, SystemName, SystemVersion, Model, BatteryState, BatteryLevel}) =>
+                    <LinkContainer key={{deviceID}} to={`/devices/${deviceID}`}>
+                        <ListGroup.Item action>
+                            <span className="font-weight-bold">
+                                {Name}
+                            </span>
+                        </ListGroup.Item>
+                    </LinkContainer>
+                )}
+            </>
+
+        )
     }
 
     function renderLander() {
@@ -26,47 +71,23 @@ function HomePage() {
 
     function renderDevices() {
         return (
-            <div className="container card">
-                <div className="row card-header table-header pt-2 pb-2">
-                    <div className="col-12 col-md-6">
+            <div className="container">
+                <div className="card">
+                    <div className="row  pt-2 pb-2">
+                        <div className="col-12 col-md-6">
 
+                        </div>
+                        <div className="col-12 col-md-6 text-right ">
+                            <button className="btn btn-success m-1 ">Dodaj nowy</button>
+                            <button className="btn btn-danger m-1">Usuń zaznaczone</button>
+                        </div>
                     </div>
-                    <div className="col-12 col-md-6 text-right ">
-                        <button className="btn btn-success m-1 ">Dodaj nowy</button>
-                        <button className="btn btn-danger m-1">Usuń zaznaczone</button>
-                    </div>
-                </div>
-                <div className="row card-body border-bottom">
-                    <div className="col-3">
-                        test
-                    </div>
-                    <div className="col-3">
-                        test
-                    </div>
-                    <div className="col-3">
-                        test
-                    </div>
-                    <div className="col-3">
-
-                    </div>
-                </div>
-                <div className="row card-body border-bottom">
-                    <div className="col-3">
-                        test
-                    </div>
-                    <div className="col-3">
-                        test
-                    </div>
-                    <div className="col-3">
-                        test
-                    </div>
-                    <div className="col-3">
-
-                    </div>
+                    <div>{!isLoading && renderDevicesList(devices)}</div>
                 </div>
             </div>
         )
     }
+
     return (
         <div className="Home">
             {isAuthenticated ? renderDevices() : renderLander()}
