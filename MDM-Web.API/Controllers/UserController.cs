a2cb0df6813a7bde8c1d4bd9abc76b188b62ace2
Filip Ceglik <fceglik@gmail.com>
2021-06-27 12:17:50 +0200

    Implemented user login

--- MDM-Web.API/Controllers/UserController.cs
+++ MDM-Web.API/Controllers/UserController.cs
@@ -70,5 +70,52 @@ namespace MDM_Web.API.Controllers
             await _userRepository.Delete(user);
             return Ok();
         }
+        
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromForm] LoginViewModel loginViewModel)
+        {
+            var user = await _userRepository.GetUser(loginViewModel.UserName);
+
+            if (user is null)
+            {
+                return BadRequest();
+            }
+            if (!BCrypt.Net.BCrypt.Verify(loginViewModel.Password,user.Password))
+            {
+                return BadRequest();
+            }
+
+            
+
+            var key = Encoding.UTF8.GetBytes(_appSettings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, loginViewModel.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role, user.Role),
+                }),
+                Expires = DateTime.UtcNow.AddSeconds(_appSettings.LifetimeInSeconds),
+                SigningCredentials = new SigningCredentials
+                (
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
+                Issuer = _appSettings.ValidIssuer
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return Ok
+            (
+                new {access_token = tokenHandler.WriteToken(token)}
+            );
+        }
+        
     }
 }
